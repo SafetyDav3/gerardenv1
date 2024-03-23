307a1eae4b494a485863074f2e3f6949a7a3dc4b
David Williams <85596817+SafetyDav3@users.noreply.github.com>
2024-03-23 17:29:49 -0500

    hero text / button edits

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -159,27 +159,25 @@ export default function Example() {
               <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
                 <div className="relative w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
                   <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                    We’re changing the way people connect.
+                    Welcome to Gerarden Fabrication
                   </h1>
                   <p className="mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
-                    Cupidatat minim id magna ipsum sint dolor qui. Sunt sit in
-                    quis cupidatat mollit aute velit. Et labore commodo nulla
-                    aliqua proident mollit ullamco exercitation tempor. Sint
-                    aliqua anim nulla sunt mollit id pariatur in voluptate
-                    cillum.
+                    Explore our fabrication tables and accessories to help you
+                    get the job done. Our laser-cut steel fabrication tables are
+                    designed to be durable and versatile.
                   </p>
                   <div className="mt-10 flex items-center gap-x-6">
                     <a
                       href="#"
                       className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
-                      Get started
+                      Order Now
                     </a>
                     <a
                       href="#"
-                      className="text-sm font-semibold leading-6 text-gray-900"
+                      className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                     >
-                      Live demo <span aria-hidden="true">→</span>
+                      Demo Video <span aria-hidden="true">→</span>
                     </a>
                   </div>
                 </div>
