6d013ffb1890daa6a90e3e1923cba972662f804e
David Williams <85596817+SafetyDav3@users.noreply.github.com>
2024-04-01 20:36:01 -0500

    vid now opens new window

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -171,6 +171,7 @@ export default function Example() {
                       Order Now
                     </a>
                     <a
+                      target="_blank"
                       href={vidReview} // Link to youtube video
                       className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                     >
