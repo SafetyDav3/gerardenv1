26708d511a70bc70d7bd6c9d30eaa0c57645fabc
David Williams <85596817+SafetyDav3@users.noreply.github.com>
2024-04-01 20:18:37 -0500

    header added

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         {children}
       </body>
     </html>
