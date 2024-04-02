1b50448cb61c7d38bd29a5615e9cdf5903276e6b
David Williams <85596817+SafetyDav3@users.noreply.github.com>
2024-04-01 20:30:45 -0500

    added video link to hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -170,7 +170,7 @@ export default function Example() {
                       Order Now
                     </a>
                     <a
-                      href="#"
+                      href="https://www.youtube.com/watch?v=TliWp-YwlMc" // Link to youtube video
                       className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                     >
                       Demo Video <span aria-hidden="true">→</span>
